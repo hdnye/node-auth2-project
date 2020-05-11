a496fcb56b25449ddee931c64cc0816220349990
Heather Nye <heatherdnye@gmail.com>
2020-05-11 17:15:14 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require('./server');
+
+port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const db = require('./data/config');
+const cors = require('cors')
+const helmet = require('helmet')
+const session = require('express-session')
+const KnexSessionStore = require('connect-session-knex');
+const usersRouter = require('./data/users/usersRouter');
+const authRouter = require('./data/auth/authRouter')
+
+const server = express()
+
+//middleware
+server.use(express.json());
+server.use(cors())
+server.use(helmet())
+server.use(session({
+    name: 'token',
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true
+    },
+    store: new KnexSessionStore({
+        knex: db,
+        createTable: true,
+    }),
+}))
+//routers
+
+//welcome message
+server.get('/', (req, res, next) => {
+    res.json({
+        message: 'Welcome to my API'
+    })
+})
+
+//error message
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: 'Unable to complete request'
+    })
+})
+
+module.exports = server;
